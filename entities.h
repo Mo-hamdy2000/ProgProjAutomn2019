0acff8b426db1c0977da53e762c67e17549245f3
Mo-hamdy2000 <53626982+Mo-hamdy2000@users.noreply.github.com>
2019-12-08 05:28:13 +0200

    Update and rename box.h to entities.h

--- entities.h
+++ entities.h
@@ -1,3 +1,4 @@
+#define StringSize 20
 typedef struct {
 	int upperSide;
 	int lowerSide;
@@ -6,3 +7,8 @@ typedef struct {
 	int remainingLines;
 	int boxColor;
 } Box;
+typedef struct {
+	char name[StringSize];
+	int score;
+	int noOfMoves;
+} Player;
