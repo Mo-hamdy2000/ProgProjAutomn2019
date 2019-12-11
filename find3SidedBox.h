bab47d42bf299ee5c877232eddc3ae9ce6eb12c9
ShehabMohamed21 <53667053+ShehabMohamed21@users.noreply.github.com>
2019-12-11 14:38:42 +0200

    Update find3SidedBox.h

--- find3SidedBox.h
+++ find3SidedBox.h
@@ -1,15 +1,43 @@
-Point find3SidedBox (int size , Box Boxes [size][size]){
-	Point point;
-	for(int i=0 ; i<size ; i++){
-		for (int j=0 ; j<size ; j++){
+int find3SidedBox (int size , Box Boxes [size][size], Point points[]){
+	int row;
+	int col;
+	int isFound = 0;
+	for(int i=0 ; i<size && !isFound ; i++){
+		for (int j=0 ; j<size && !isFound ; j++){
 			if(Boxes[i][j].remainingLines==1){
-				point.x = i;
-				point.y = j;
-				return point;
+				isFound = 1;
+				row = i;
+				col = j;
 			}
 		}
 	}
-	point.x=-1;
-	point.y=-1;
-return point;
+	if(isFound)
+	{
+		if(Boxes[row][col].upperSide==1){
+			points[0].x = col;
+			points[0].y = row;
+			points[1].x = col+1;
+			points[1].y = row;
+		}
+		else if (Boxes[row][col].lowerSide==1){
+			points[0].x = col;
+			points[0].y = row+1;
+			points[1].x = col+1;
+			points[1].y = row+1;
+		}
+		else if (Boxes[row][col].rightSide==1){
+			points[0].x = col+1;
+			points[0].y = row;
+			points[1].x = col+1;
+			points[1].y = row;
+		}
+		else{
+			points[0].x = col;
+			points[0].y = row;
+			points[1].x = col;
+			points[1].y = row+1;
+			}
+		return 1;
+	}
+	return 0;
 }
